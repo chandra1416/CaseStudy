0292ac00d05598b59353799327c55d022fc674e0
chandra1416 <58411134+chandra1416@users.noreply.github.com>
2019-12-16 01:42:28 +0530

    policy-Function for Converting string to date

--- policy.py
+++ policy.py
@@ -2,3 +2,10 @@ import mysql.connector
 from mysql.connector import Error
 con=mysql.connector.connect(host="localhost",username="chandra",password="@chennai1",database='abc')
 cur=con.cursor()
+#Function for Converting string to date
+def date_convert(x):
+	format='%d-%b-%Y'
+	datetime_object=datetime.datetime.strptime(x,format)
+	h=datetime_object.date()
+	#d=datetime.datetime.strftime(h,format)
+	return h
