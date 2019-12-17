9db362a30a86a50b62d97fac937f7c61aa9ed3a7
chandra1416 <58411134+chandra1416@users.noreply.github.com>
2019-12-17 10:59:28 +0530

    Add multiple primary key to policy table creation

--- Database.sql
+++ Database.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS Healthcare;
 USE DATABASE Healthcare;
 
 -- Create Policy Table 
-CREATE TABLE IF NOT EXISTS Policy(PolicyId BIGINT NOT NULL,PolicyNumber INT PRIMARY KEY ,PolicYHolderName VARCHAR(20)NOT NULL,PolicyStartDate DATE NOT NULL,PolicyExpirationDate DATE NOT NULL,PolicyType VARCHAR(25)NOT NULL)engine=InnoDB;;
+CREATE TABLE IF NOT EXISTS Policy(PolicyId BIGINT,PolicyNumber INT,PolicYHolderName VARCHAR(20)NOT NULL,PolicyStartDate DATE NOT NULL,PolicyExpirationDate DATE NOT NULL,PolicyType VARCHAR(25)NOT NULL,PRIMARY KEY(PolicyId,PolicyNumber))engine=InnoDB;
 
 --Create CLAIMS Table
 CREATE TABLE IF NOT EXISTS claims(PolicyId BIGINT  ,PolicyNumber INT ,ClaimID INT PRIMARY KEY,ClaimType VARCHAR(20),ClaimApplyDate DATE,ClaimServiceDate DATE,ClaimStatus VARCHAR(10),ClaimCompletionDate DATE,CONSTRAINT `fk_ciaim` FOREIGN KEY (PolicyNumber)REFERENCES policy(PolicyNumber)ON UPDATE CASCADE ON DELETE RESTRICT )engine=InnoDB;
